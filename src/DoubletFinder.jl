ca4beff30e8e1366e7a4221c845934f5db6e4a75
Sameer <sameeresque@protonmail.com>
2019-04-11 12:15:08 -0400

    Update DoubletFinder.jl

--- src/DoubletFinder.jl
+++ src/DoubletFinder.jl
@@ -197,8 +197,9 @@ end
     function atomicdata(all_ions::Array{String,1}=["NV","CIV","SiIV","OVI","MgII","MgI","FeII","AlII","AlIII","MnII","CrII","ZnII","Lya","HI","NeVIII"])
         
         # The data file containing atomic information of interest
-        path=dirname(pathof(DoubletFinder))
+        path=dirname(pathof(DoubletFinder))       
         path_atomicfile=joinpath(path,"/atoms_UVES.csv")
+        println(path_atomicfile)
         @assert isfile(path_atomicfile)
         df_atomic_data=CSV.read(path_atomicfile,header=["Ion","Rest_Wavelength","f","Gamma","Mass","Column6"],delim='\t')
 
